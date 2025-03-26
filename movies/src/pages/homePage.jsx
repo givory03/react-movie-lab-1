6747b4a36894691fa08162ebdd8f44f818c78f63
Genevieve <20102601@mail.wit.ie>
2025-03-26 12:42:46 +0000

    Movie card component created

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Grid } from '@mui/material';
+import React from "react";
+import Grid from "@mui/material/Grid2";
+import MovieCard from '../components/movieCard'
 
 const HomePage = (props) => {
-    const movies = props?.movies || [];
-
-    return (
-        <Grid container>
-            <Grid item xs={12}>
-                <h1>HomePage</h1>
-                <p>This page is rendering correctly!</p>
-            </Grid>
-        </Grid>
-    );
+  const movies = props.movies;
+    
+  return (
+  <Grid container>
+      <Grid size={12}>
+          <h1> HomePage </h1>
+      </Grid>
+      <Grid size={3}>
+        <MovieCard movie={movies[0]} />
+      </Grid>
+    </Grid>
+  );
 };
-
 export default HomePage;
